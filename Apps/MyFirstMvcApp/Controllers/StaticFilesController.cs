82c84ff20dfd44c053f13a4b97cf58b6dcd85359
Teodor Levakov <t.levakov@abv.bg>
2020-10-06 15:48:19 +0200

    Introduce controllers

---
+++ Apps/MyFirstMvcApp/Controllers/StaticFilesController.cs
@@ -0,0 +1,22 @@
+﻿using SUS.HTTP;
+using SUS.MvcFramework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MyFirstMvcApp.Controllers
+{
+    public class StaticFilesController : Controller
+    {
+        public HttpResponse Favicon(HttpRequest request)
+        {
+            var fileBytes = File.ReadAllBytes("wwwroot/favico.ico");
+
+            var response = new HttpResponse("image/vnd.microsoft.icon", fileBytes);
+
+            return response;
+        }
+
+    }
+}
--- Apps/MyFirstMvcApp/Program.cs
+++ Apps/MyFirstMvcApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using SUS.HTTP;
+﻿using MyFirstMvcApp.Controllers;
+using SUS.HTTP;
 using System;
 using System.IO;
 using System.Linq;
@@ -18,51 +19,20 @@ namespace MyFirstMvcApp
             //{
             //    return new HttpResponse();
             //});
-            server.AddRoute("/", HomePage);
-            server.AddRoute("/favicon.ico", Favicon);
-            server.AddRoute("/about", About);
-            server.AddRoute("/users/login", Login);
+            server.AddRoute("/", new HomeController().Index);
+            server.AddRoute("/favicon.ico", new StaticFilesController().Favicon);
+            server.AddRoute("/about", new HomeController().About);
+            server.AddRoute("/users/login", new UsersController().Login);
+            server.AddRoute("/users/register", new UsersController().Register);
             await server.StartAsync(80);
         }
 
-        static HttpResponse HomePage(HttpRequest request) 
-        {
-            var responseHtml = "<h1>Welcome!</h1>" + request.Headers.FirstOrDefault(x => x.Name == "User-Agent")?.Value;
-            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+        
 
-            var response = new HttpResponse("text/html", responseBodyBytes);
+        
 
-            return response;
+        
 
-        }
-
-        static HttpResponse Favicon(HttpRequest request)
-        {
-            var fileBytes = File.ReadAllBytes("wwwroot/favico.ico");
-
-            var response = new HttpResponse("image/vnd.microsoft.icon", fileBytes);
-
-            return response;
-        }
-
-        static HttpResponse About(HttpRequest request)
-        {
-            var responseHtml = "<h1>About...!</h1>";
-            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
-
-            var response = new HttpResponse("text/html", responseBodyBytes);
-
-            return response;
-        }
-
-        static HttpResponse Login(HttpRequest request)
-        {
-            var responseHtml = "<h1>Login...!</h1>";
-            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
-
-            var response = new HttpResponse("text/html", responseBodyBytes);
-
-            return response;
-        }
+        
     }
 }
